8e07f8f42e607d8005bea2523d83a9caf22d7c30
ilhamhadisyah <ilhamhadisyahsmd@gmail.com>
2021-03-22 16:13:32 +0700

    first

--- app/src/main/java/com/ilham/actionbar/MainActivity.kt
+++ app/src/main/java/com/ilham/actionbar/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.ilham.actionbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.ilham.actionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu1 ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container,MenuFragment()).addToBackStack(null).commit()
+                return true
+            }
+            R.id.menu2 ->{
+                val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> return true
+        }
     }
 }
